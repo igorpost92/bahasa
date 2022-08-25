488ad5dd0cd22d8baab29b748fc53007d31dbdb7
ipostolenko <ipostolenko@techmategroup.com>
2022-08-25 20:20:56 +0800

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 120,
+  trailingComma: 'all',
+  htmlWhitespaceSensitivity: 'strict',
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'avoid',
+};
