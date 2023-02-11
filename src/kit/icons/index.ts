c8751e99e63f2653cf24612449524a67d36462f1
Igor Postolenko <ipostolenko@techmategroup.com>
2023-02-11 10:54:11 +0800

    icons

--- src/kit/icons/index.ts
+++ src/kit/icons/index.ts
@@ -1,4 +1,7 @@
 export { default as ChevronDownIcon } from './chevron-down.svg';
+export { default as ChevronLeftIcon } from './chevron-left.svg';
+export { default as ChevronRightIcon } from './chevron-right.svg';
+export { default as ChevronUpIcon } from './chevron-up.svg';
 export { default as CloseIcon } from './close.svg';
 export { default as SpinnerIcon } from './spinner.svg';
 export { default as SettingsIcon } from './settings.svg';
