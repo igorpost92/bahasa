dd0d52745b265ba2b714237a90a0efa751916f25
Igor Postolenko <ipostolenko@techmategroup.com>
2023-03-24 13:13:35 +0800

    fix modal ios

--- src/kit/components/Drawer/Drawer.tsx
+++ src/kit/components/Drawer/Drawer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import cn from 'classnames';
 import styles from './Drawer.module.scss';
 import { Portal } from '../Portal/Portal';
-import { useScrollLock } from '../../hooks';
+import { ALLOW_INSIDE_SCROLL_CLASSNAME, useScrollLock } from '../../hooks';
 
 interface Props {
   className?: string;
@@ -14,7 +14,10 @@ interface Props {
   // canClose?: boolean;
 
   // TODO: position left
+  // position?: 'left' | 'bottom'
 }
+
+// TODO: close with swipe
 
 export const Drawer: React.FC<Props> = props => {
   useScrollLock(props.isOpen);
@@ -37,7 +40,7 @@ export const Drawer: React.FC<Props> = props => {
           }}
         />
         <div
-          className={cn(styles.content, props.className, {
+          className={cn(styles.content, props.className, ALLOW_INSIDE_SCROLL_CLASSNAME, {
             [styles.l]: size === 'l',
             [styles.xl]: size === 'xl',
           })}
--- src/kit/hooks/useScrollLock/index.tsx
+++ src/kit/hooks/useScrollLock/index.tsx
@@ -1,23 +1,37 @@
 import { useEffect, useRef } from 'react';
 import styles from './styles.module.scss';
 
+export const ALLOW_INSIDE_SCROLL_CLASSNAME = 'allow-inside-scroll-classname';
+
+const handler = (e: TouchEvent) => {
+  const inModal = (e.target as HTMLElement).closest(`.${ALLOW_INSIDE_SCROLL_CLASSNAME}`);
+  console.log(e.target, (e.target as HTMLElement).parentElement, inModal);
+  if (!inModal) {
+    e.preventDefault();
+  }
+};
+
 export const useScrollLock = (enabled: boolean) => {
-  const scrollPos = useRef(0);
+  // const scrollPos = useRef(0);
 
   useEffect(() => {
     if (!enabled) {
       return;
     }
 
-    const scrollTop = window.scrollY;
-    scrollPos.current = scrollTop;
+    document.addEventListener('touchmove', handler, { passive: false });
+
+    // const scrollTop = window.scrollY;
+    // scrollPos.current = scrollTop;
     document.body.classList.add(styles.preventBodyScroll);
-    document.body.style.top = `-${scrollTop}px`;
+    // document.body.style.top = `-${scrollTop}px`;
 
     return () => {
+      document.removeEventListener('touchmove', handler);
+
       document.body.classList.remove(styles.preventBodyScroll);
-      document.body.style.top = '';
-      window.scroll({ top: scrollPos.current });
+      // document.body.style.top = '';
+      // window.scroll({ top: scrollPos.current });
     };
   }, [enabled]);
 };
