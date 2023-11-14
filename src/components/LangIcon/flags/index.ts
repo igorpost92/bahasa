c1bedc9294a23415817210f573c6000dd737ef68
Igor Postolenko <ipostolenko@techmategroup.com>
2023-11-14 23:47:14 +0300

    add eng icon

--- src/components/LangIcon/flags/index.ts
+++ src/components/LangIcon/flags/index.ts
@@ -1,7 +1,9 @@
 import { default as ID } from './id.svg';
 import { default as ES } from './es.svg';
+import { default as EN } from './en.svg';
 
 export default {
   ES,
   ID,
+  EN,
 };
--- src/storage/hooks/languages.ts
+++ src/storage/hooks/languages.ts
@@ -1,6 +1,7 @@
 const languages = [
   { code: 'ID', name: 'Bahasa Indonesia' },
   { code: 'ES', name: 'Español' },
+  { code: 'EN', name: 'English' },
 ];
 
 export const useLanguages = () => {
