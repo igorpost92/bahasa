62e9b33ae059f2aba1d62cdad6602bd709734cff
Igor Postolenko <ipostolenko@techmategroup.com>
2023-11-28 21:26:22 +0300

    refacto login

--- src/constants/localStorageTokens.ts
+++ src/constants/localStorageTokens.ts
@@ -1,4 +1,6 @@
 export enum LocalStorageTokens {
+  UserName = 'LS_USER_NAME',
+  UserPassword = 'LS_USER_PASSWORD',
   SessionID = 'LS_SESSION_ID',
   RandomConjugationSettings = 'LS_RANDOM_CONJUGATION_SETTINGS',
   CurrentVersion = 'LS_CURRENT_VERSION',
--- src/pages/Settings/Login/Login.tsx
+++ src/pages/Settings/Login/Login.tsx
@@ -4,21 +4,40 @@ import { ControlGroup, Input, Button } from '../../../kit';
 import { AppPage } from '../../../components/AppPage/AppPage';
 import { useNavigate } from 'react-router-dom';
 import { getSupabaseKey, updateSupabaseKey } from '../../../api/sendRequest';
+import { LocalStorageTokens } from '../../../constants/localStorageTokens';
+import { authApi } from '../../../api2';
+import { AxiosError } from 'axios';
 
-// TODO: form
+const getKey = () => getSupabaseKey() ?? '';
+const getName = () => localStorage.getItem(LocalStorageTokens.UserName) ?? '';
+const getPassword = () => localStorage.getItem(LocalStorageTokens.UserPassword) ?? '';
 
 const Login: React.FC = () => {
   const navigate = useNavigate();
 
-  const [key, setKey] = useState(() => getSupabaseKey() ?? '');
+  const [key, setKey] = useState(getKey);
+  const [name, setName] = useState(getName);
+  const [password, setPassword] = useState(getPassword);
 
-  const save = () => {
+  const save = async () => {
     if (!key) {
       return;
     }
 
-    updateSupabaseKey(key);
-    navigate('/settings');
+    try {
+      await authApi.signIn(name, password);
+
+      localStorage.setItem(LocalStorageTokens.UserName, name);
+      localStorage.setItem(LocalStorageTokens.UserPassword, password);
+      updateSupabaseKey(key);
+      navigate('/settings');
+    } catch (e) {
+      let message;
+      if (e instanceof AxiosError) {
+        message = e.response?.data?.message;
+      }
+      alert(message ?? 'Error during sign in');
+    }
   };
 
   return (
@@ -27,11 +46,19 @@ const Login: React.FC = () => {
       headerRight={false}
       contentClassName={styles.pageWrap}
     >
-      <div className={styles.form}>
-        <ControlGroup id={'key'} label={'Key'}>
-          <Input value={key} onChange={setKey} />
-        </ControlGroup>
-      </div>
+      <ControlGroup id={'key'} label={'Key'}>
+        <Input value={key} onChange={setKey} />
+      </ControlGroup>
+      <br />
+      <br />
+      <ControlGroup id={'key'} label={'Username'}>
+        <Input value={name} onChange={setName} />
+      </ControlGroup>
+      <br />
+      <ControlGroup id={'key'} label={'Password'}>
+        <Input type={'password'} value={password} onChange={setPassword} />
+      </ControlGroup>
+      <br />
       <Button className={styles.btnWrap} intent={'primary'} onClick={save}>
         Login
       </Button>
